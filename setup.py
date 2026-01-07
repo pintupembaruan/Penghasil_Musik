18e1a5be31abf2e7fe767ec385a3b5b73b7633d9
pintupembaruan <pintupembaruan@gmail.com>
2026-01-07 14:23:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="islamic-music-bot",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=[
-        # Dependensi utama sudah dihandle di workflow untuk kestabilan
+        # Dependensi utama sudah dihandle di workflow yml 
+        # untuk menghindari error build wheel
     ],
 )
