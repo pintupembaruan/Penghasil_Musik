8d1a8d12d62085e9dec7ace2d901690661cde46b
pintupembaruan <pintupembaruan@gmail.com>
2026-01-07 14:05:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version="1.1.0",
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'torchaudio',
-        'audiocraft',
+        # Dependensi utama sudah dihandle di workflow untuk kestabilan
     ],
-    description="Bot penghasil musik Islami original",
 )
